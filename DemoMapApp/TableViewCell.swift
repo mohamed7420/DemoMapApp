a3c0d37a66bbdbc9b048789715560402a1f3fc5f
mohamedosama2074 <iosengks74@gmail.com>
2019-09-04 01:41:17 +0200

    DemoMapApp

---
+++ DemoMapApp/TableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell.swift
+//  DemoMapApp
+//
+//  Created by Mohamed on 9/4/19.
+//  Copyright © 2019 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
